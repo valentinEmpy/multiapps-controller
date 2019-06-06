c9667033308eb909363a6162f47f36e37bccd611
Boyadzhiev, Petko <p.boyadzhiev@sap.com>
2019-06-06 11:19:04 +0300

    Metadata mtas api adoption

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/MtaConfigurationPurger.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/MtaConfigurationPurger.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import com.sap.cloud.lm.sl.cf.core.auditlogging.AuditLoggingProvider;
 import com.sap.cloud.lm.sl.cf.core.cf.detect.ApplicationMtaMetadataParser;
+import com.sap.cloud.lm.sl.cf.core.cf.detect.mapping.AppMetadataMapper;
 import com.sap.cloud.lm.sl.cf.core.message.Messages;
 import com.sap.cloud.lm.sl.cf.core.model.ApplicationMtaMetadata;
 import com.sap.cloud.lm.sl.cf.core.model.CloudTarget;
@@ -32,12 +33,14 @@ public class MtaConfigurationPurger {
     private final CloudControllerClient client;
     private final ConfigurationEntryService configurationEntryService;
     private final ConfigurationSubscriptionService configurationSubscriptionService;
+    private AppMetadataMapper appMetadataMapper;
 
     public MtaConfigurationPurger(CloudControllerClient client, ConfigurationEntryService configurationEntryService,
-                                  ConfigurationSubscriptionService configurationSubscriptionService) {
+                                  ConfigurationSubscriptionService configurationSubscriptionService, AppMetadataMapper appMetadataMapper) {
         this.client = client;
         this.configurationEntryService = configurationEntryService;
         this.configurationSubscriptionService = configurationSubscriptionService;
+        this.appMetadataMapper = appMetadataMapper;
     }
 
     public void purge(String org, String space) {
@@ -106,14 +109,24 @@ public class MtaConfigurationPurger {
     }
 
     private List<ConfigurationEntry> getStillRelevantConfigurationEntries(CloudApplication app) {
-        ApplicationMtaMetadata metadata = ApplicationMtaMetadataParser.parseAppMetadata(app);
+        getApplicationMtaMetadata(app);
+        ApplicationMtaMetadata metadata = getApplicationMtaMetadata(app);
         if (metadata == null) {
             return Collections.emptyList();
         }
-        return metadata.getProvidedDependencyNames()
+        return metadata.getModule()
+                       .getProvidedDependencyNames()
                        .stream()
                        .map(providedDependencyName -> toConfigurationEntry(metadata.getMtaMetadata(), providedDependencyName))
                        .collect(Collectors.toList());
+    }
+
+    private ApplicationMtaMetadata getApplicationMtaMetadata(CloudApplication app) {
+        if (app.getMetadata() == null) {
+            return ApplicationMtaMetadataParser.parseAppMetadata(app);
+        } else {
+            return appMetadataMapper.mapMetadata(app);
+        }
     }
 
     private ConfigurationEntry toConfigurationEntry(DeployedMtaMetadata metadata, String providedDependencyName) {
