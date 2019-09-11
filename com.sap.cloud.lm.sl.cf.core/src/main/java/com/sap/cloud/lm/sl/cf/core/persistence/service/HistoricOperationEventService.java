0bebfad83d2c82bc86718b8f784bbef45ecd7892
Velkov <valentin.velkov@sap.com>
2019-09-11 09:35:18 +0300

    Introduce HistoricOperationEventService & Query

---
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/persistence/service/HistoricOperationEventService.java
@@ -0,0 +1,50 @@
+package com.sap.cloud.lm.sl.cf.core.persistence.service;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cloud.lm.sl.cf.core.message.Messages;
+import com.sap.cloud.lm.sl.cf.core.model.HistoricOperationEvent;
+import com.sap.cloud.lm.sl.cf.core.persistence.dto.HistoricOperationEventDto;
+import com.sap.cloud.lm.sl.cf.core.persistence.object.factory.HistoricOperationEventFactory;
+import com.sap.cloud.lm.sl.cf.core.persistence.object.factory.PersistenceObjectFactory;
+import com.sap.cloud.lm.sl.cf.core.persistence.query.HistoricOperationEventQuery;
+import com.sap.cloud.lm.sl.cf.core.persistence.query.impl.HistoricOperationEventQueryImpl;
+import com.sap.cloud.lm.sl.common.ConflictException;
+import com.sap.cloud.lm.sl.common.NotFoundException;
+
+@Component
+public class HistoricOperationEventService extends PersistenceService<HistoricOperationEvent, HistoricOperationEventDto, Long> {
+
+    @Inject
+    private HistoricOperationEventFactory historicOperationEventFactory;
+
+    @Inject
+    public HistoricOperationEventService(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
+    }
+
+    public HistoricOperationEventQuery createQuery() {
+        return new HistoricOperationEventQueryImpl(createEntityManager(), historicOperationEventFactory);
+    }
+
+    @Override
+    protected PersistenceObjectFactory<HistoricOperationEvent, HistoricOperationEventDto> getPersistenceObjectFactory() {
+        return historicOperationEventFactory;
+    }
+
+    @Override
+    protected void onEntityConflict(HistoricOperationEventDto dto, Throwable t) {
+        throw (ConflictException) new ConflictException(Messages.HISTORIC_OPERATION_EVENT_ALREADY_EXISTS,
+                                                        dto.getProcessId(),
+                                                        dto.getPrimaryKey()).initCause(t);
+    }
+
+    @Override
+    protected void onEntityNotFound(Long id) {
+        throw new NotFoundException(Messages.HISTORIC_OPERATION_EVENT_NOT_FOUND, id);
+    }
+
+}
